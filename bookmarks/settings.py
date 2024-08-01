7c236fed59593a876aec4b5a47f18d12d0667bd1
IMOWWW <imowww@yandex.ru>
2024-08-01 16:37:36 +0500

    add pipeline for create profile social auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -114,6 +114,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SOCIAL_AUTH_PIPELINE = [ 
+    'social_core.pipeline.social_auth.social_details', 
+    'social_core.pipeline.social_auth.social_uid', 
+    'social_core.pipeline.social_auth.auth_allowed', 
+    'social_core.pipeline.social_auth.social_user', 
+    'social_core.pipeline.user.get_username', 
+    'social_core.pipeline.user.create_user', 
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user', 
+    'social_core.pipeline.social_auth.load_extra_data', 
+    'social_core.pipeline.user.user_details',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
